582ae2e91f1713671f4fd071a9f2d1b4ffdd4220
Adrian <a.roszak@josera.pl>
2022-09-11 10:53:09 +0200

    config: base configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,10 @@ module.exports = {
   rules: {
     'node/no-missing-import': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/no-unescaped-entities': 'off',
   },
 };
